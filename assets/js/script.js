617c7fd1cb14d155e6c004823522b369b3ec7862
Dhonata Chaves Santos <dhonatachaves7@gmail.com>
2025-10-18 21:13:32 -0300

    feat(products): Adiciona função JS para gerar e injetar cards no DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,41 @@
 const menuMobile = document.querySelector('.menu-mobile');
 const itensMenu = document.querySelector('.itens-menu');
 const menuMobileMmLine = document.querySelectorAll('.mm-line');
+const listaDeVinhos = [
+    {
+        id: 1,
+        nome: "Château Reserva Especial",
+        preco: 2500,
+        imagem: "assets/images/vinho-1.jpeg",
+        descricao: "Um tinto robusto com notas de carvalho e frutas vermelhas."
+    },
+    {
+        id: 2,
+        nome: "Espumante Brut Rosé",
+        preco: 1400,
+        imagem: "assets/images/vinho-2.jpeg",
+        descricao: "Leve e frutado, ideal para celebrações. Experimente o melhor!"
+    },
+    {
+        id: 3,
+        nome: "Espumante Galax Luanny",
+        preco: 1500,
+        imagem: "assets/images/vinho-3.jpeg",
+        descricao: "Irresístivel, a prova que o sabor perfeito existe. Aproveite!"
+    },
+    {
+        id: 1,
+        nome: "Château Reserva Especial",
+        preco: 2500,
+        imagem: "assets/images/vinho-1.jpeg",
+        descricao: "Um tinto robusto com notas de carvalho e frutas vermelhas."
+    }
+
+];
 
 
 menuMobile.addEventListener('click', showMenu);
+
 
 
 
@@ -25,5 +57,22 @@ function showMenu(e) {
 
 
 
+function gerarCardsDeProdutos() {
+    const gridContainer = document.querySelector('.product-grid');
+    let htmlContent = '';
 
+    listaDeVinhos.forEach(vinho => {
+        const cardHTML = `<div class="product-card" data-product-id="${vinho.id}">
+            <img src="${vinho.imagem}" alt="${vinho.nome}">
+            <h3>${vinho.nome}</h3>
+            <p class="description"> ${vinho.descricao}</p>
+            <span class="price"> R$ ${vinho.preco.toFixed(2).replace('.', ',')}</span>
+            <button class="add-to-cart-btn" data-id="${vinho.id}"> Adicionar </button>
+        </div> `;
 
+        htmlContent += cardHTML; // Concatena o HTML de cada card
+    });
+    gridContainer.innerHTML = htmlContent;
+}
+
+document.addEventListener('DOMContentLoaded', gerarCardsDeProdutos);
